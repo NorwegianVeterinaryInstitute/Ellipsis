d5902f696090973f3b5099902bb9f522eb167ada
Håkon Kaspersen <hakon.kaspersen@vetinst.no>
2020-08-12 12:51:49 +0200

    added columns in output report

--- bin/collate_data.R
+++ bin/collate_data.R
@@ -6,8 +6,7 @@ library(funtools)
 library(tidyr)
 
 mobtyper_reports <- get_data(".",
-                             pattern = "mobtyper",
-			     convert = TRUE) %>%
+                             pattern = "mobtyper") %>%
   mutate(ref = sub("_mobtyper", "", ref),
          ref = sub(".fasta_report.txt", "", ref)) %>%
   select(-file_id)
@@ -23,14 +22,14 @@ prokka_reports <- get_data(".",
   select(ref, CDS, gene, tRNA)
 
 plasmidfinder_reports <- get_data(".",
-                                  pattern = "plasfinder",
-				  convert = TRUE) %>%
+                                  pattern = "plasfinder") %>%
   mutate(ref = sub("_plasfinder_results_tab.tsv", "", ref)) %>%
   group_by(ref) %>%
   summarise_all(list(func_paste)) %>%
-  select(ref, Plasmid, Identity) %>%
+  select(ref, Plasmid, Identity, Contig) %>%
   rename("pf_replicon" = Plasmid,
-         "pf_replicon_identity" = Identity)
+         "pf_replicon_identity" = Identity,
+         "pf_contig" = Contig)
 
 resfinder_reports <- get_data(".",
                               pattern = "resfinder",
@@ -38,9 +37,10 @@ resfinder_reports <- get_data(".",
   mutate(ref = sub("_resfinder_results_tab.tsv", "", ref)) %>%
   group_by(ref) %>%
   summarise_all(list(func_paste)) %>%
-  select(ref, `Resistance gene`, Identity) %>%
+  select(ref, `Resistance gene`, Identity, Contig) %>%
   rename("rf_gene" = `Resistance gene`,
-         "rf_gene_identity" = Identity)
+         "rf_gene_identity" = Identity,
+         "rf_contig" = Contig)
 
 virfinder_reports <- get_data(".",
                               pattern = "virfinder",
@@ -48,15 +48,19 @@ virfinder_reports <- get_data(".",
   mutate(ref = sub("_virfinder_results_tab.tsv", "", ref)) %>%
   group_by(ref) %>%
   summarise_all(list(func_paste)) %>%
-  select(ref, `Virulence factor`, Identity) %>%
+  select(ref, `Virulence factor`, Identity, Contig) %>%
   rename("vf_gene" = `Virulence factor`,
-         "vf_gene_identity" = Identity)
+         "vf_gene_identity" = Identity,
+         "vf_contig" = Contig)
 
 total_data <- prokka_reports %>%
   left_join(mobtyper_reports, by = "ref") %>%
   left_join(plasmidfinder_reports, by = "ref") %>%
   left_join(resfinder_reports, by = "ref") %>%
-  left_join(virfinder_reports, by = "ref")
+  left_join(virfinder_reports, by = "ref") %>%
+  mutate(sample = sub("(.*?)_plasmid.+", "\\1", ref),
+         plasmid = sub(".*?_(plasmid_.*?)", "\\1", ref)) %>%
+  select(sample, plasmid, everything(), -ref)
 
 write.table(total_data,
             "total_report.txt",
