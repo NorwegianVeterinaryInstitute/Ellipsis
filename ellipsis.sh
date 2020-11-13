be83d465c05b63ab1f3267aa9cd9e76f4ce969f9
Håkon Kaspersen <hakon.kaspersen@vetinst.no>
2020-11-13 11:04:09 +0100

    added pipeline version output

--- ellipsis.sh
+++ ellipsis.sh
@@ -7,19 +7,19 @@ config=$2
 outdir=$3
 workdir=${4:-$USERWORK/ellipsis_work}
 
+mkdir -p ${outdir}/config_files
+cp ${config} ${outdir}/config_files
+git --git-dir ${script_directory}/.git branch -v | grep "\*" | awk '{print $2, $3}' > ${outdir}/config_files/ellipsis_version.log
+
 if [[ $track == "main" ]]; then
-	mkdir -p ${outdir}/config_files
 	cp ${script_directory}/main.nf ${outdir}/config_files
-	cp ${config} ${outdir}/config_files
 
 	module load Java/11.0.2
 	nextflow run ${script_directory}/main.nf -c ${config} --out_dir=${outdir} -work-dir ${workdir} -resume
 	module unload Java/11.0.2; fi
 
 if [[ $track == "plasmap" ]]; then
-	mkdir -p ${outdir}/config_files
         cp ${script_directory}/plasmap.nf ${outdir}/config_files
-        cp ${config} ${outdir}/config_files
 
         module load Java/11.0.2
         nextflow run ${script_directory}/plasmap.nf -c ${config} --out_dir=${outdir} -work-dir ${workdir} -resume
