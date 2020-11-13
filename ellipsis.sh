549347a3ad3ab2e9d4ad7130c8a2e840972f09bb
Håkon Kaspersen <hakon.kaspersen@vetinst.no>
2020-11-13 10:52:59 +0100

    added plasmap into same run file as main pipeline

--- ellipsis.sh
+++ ellipsis.sh
@@ -2,14 +2,25 @@
 
 script_directory=$(dirname ${BASH_SOURCE[0]})
 
-config=$1
-outdir=$2
-workdir=${3:-$USERWORK/ellipsis_work}
+track=$1
+config=$2
+outdir=$3
+workdir=${4:-$USERWORK/ellipsis_work}
 
-mkdir -p ${outdir}/config_files
-cp ${script_directory}/main.nf ${outdir}/config_files
-cp ${config} ${outdir}/config_files
+if [[ $track == "main" ]]; then
+	mkdir -p ${outdir}/config_files
+	cp ${script_directory}/main.nf ${outdir}/config_files
+	cp ${config} ${outdir}/config_files
 
-module load Java/11.0.2
-nextflow run ${script_directory}/main.nf -c ${config} --out_dir=${outdir} -work-dir ${workdir} -resume
-module unload Java/11.0.2
+	module load Java/11.0.2
+	nextflow run ${script_directory}/main.nf -c ${config} --out_dir=${outdir} -work-dir ${workdir} -resume
+	module unload Java/11.0.2; fi
+
+if [[ $track == "plasmap" ]]; then
+	mkdir -p ${outdir}/config_files
+        cp ${script_directory}/plasmap.nf ${outdir}/config_files
+        cp ${config} ${outdir}/config_files
+
+        module load Java/11.0.2
+        nextflow run ${script_directory}/plasmap.nf -c ${config} --out_dir=${outdir} -work-dir ${workdir} -resume
+        module unload Java/11.0.2; fi
