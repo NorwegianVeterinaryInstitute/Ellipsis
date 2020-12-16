d70047c10f2b3ff82487e002f81dea7c5866ab9a
hkaspersen <hakon.kaspersen@protonmail.com>
2020-12-16 12:26:41 +0100

    changed collating of data in R

--- bin/collate_plasmap.R
+++ bin/collate_plasmap.R
@@ -1,23 +1,32 @@
 #!/usr/bin/env Rscript
-library(readr)
 library(dplyr)
+library(funtools)
 library(impoRt)
-library(tidyr)
 
-data <- get_data(".",
-                 "report.txt",
-                 recursive = FALSE) %>%
-  select(-ref) %>%
-  rowwise() %>%
-  mutate(id = sub(paste0(plasmid, "_"), "", id),
-         id = sub("_mapped_sorted", "", id),
-         percent_mapped = round(percent_mapped, 3)) %>%
-  ungroup() %>%
-  spread(plasmid, percent_mapped)
+calc_cov <- function(df) {
+  df %>%
+    rename("reference" = X1,
+           "pos" = X2,
+           "reads" = X3) %>%
+    mutate(reads_no_unmapped = ifelse(reads == 0, NA, reads),
+           test = ifelse(reads == 0, 0, 1)) %>%
+    summarise(reference = unique(reference),
+              cov_perc = round(sum(test)/max(pos)*100, 2),
+              mean_reads_per_base = round(mean(reads_no_unmapped, na.rm = TRUE), 2),
+              median_reads_per_base = median(reads_no_unmapped, na.rm = TRUE),
+              sd_reads_per_base = round(sd(reads_no_unmapped, na.rm = TRUE), 2))
+}
 
-write.table(data,
+cov_data <- get_data(".",
+                     pattern = "genomecov.txt",
+                     col_names = FALSE,
+                     df = FALSE) %>%
+  lapply(., calc_cov) %>%
+  bind_rows(.id = "sample") %>%
+  mutate(sample = sub("_genomecov.txt", "", sample))
+
+write.table(cov_data,
             "plasmid_mapping_report.txt",
             sep = "\t",
             row.names = FALSE,
             quote = FALSE)
-
